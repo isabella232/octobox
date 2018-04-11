9732d0a3a57cd2f4bf9e4ae5ea83a695aac4559f
jules2689 <julian@jnadeau.ca>
2018-04-11 17:28:47 -0400

    Add sidekiq status gem

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'sidekiq-scheduler'
+require 'sidekiq-status'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Octobox.config.redis_url }
@@ -10,8 +11,11 @@ Sidekiq.configure_server do |config|
       Sidekiq::Scheduler.reload_schedule!
     end
   end
+  Sidekiq::Status.configure_server_middleware config, expiration: 60.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 60.minutes
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: Octobox.config.redis_url }
+  Sidekiq::Status.configure_client_middleware config, expiration: 60.minutes
 end
