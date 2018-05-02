80e7ffa17d5f05b85c6340611b7d9ab6b36c706f
jules2689 <julian@jnadeau.ca>
2018-05-02 15:16:53 -0400

    Make worker timeout to avoid stuck jobs

--- app/workers/sync_notifications_worker.rb
+++ app/workers/sync_notifications_worker.rb
@@ -1,21 +1,28 @@
 # frozen_string_literal: true
 
+require 'timeout'
+
 class SyncNotificationsWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
   sidekiq_options queue: :sync_notifications, unique: :until_executed
 
-  def perform(user_id)
-    user = User.find_by(id: user_id)
-    return unless user.present?
+  TIMEOUT = 30.minutes.to_i
 
-    user.sync_notifications_in_foreground
-  rescue Octokit::Unauthorized, Octokit::Forbidden => exception
-    handle_exception(exception, user)
-  rescue Octokit::BadGateway, Octokit::ServerError, Octokit::ServiceUnavailable => exception
-    handle_exception(exception, user)
-  rescue Faraday::ClientError => exception
-    handle_exception(exception, user)
+  def perform(user_id)
+    Timeout.timeout(TIMEOUT) do
+      begin
+        user = User.find_by(id: user_id)
+        return unless user.present?
+        user.sync_notifications_in_foreground
+      rescue Octokit::Unauthorized, Octokit::Forbidden => exception
+        handle_exception(exception, user)
+      rescue Octokit::BadGateway, Octokit::ServerError, Octokit::ServiceUnavailable => exception
+        handle_exception(exception, user)
+      rescue Faraday::ClientError => exception
+        handle_exception(exception, user)
+      end
+    end
   end
 
   private
