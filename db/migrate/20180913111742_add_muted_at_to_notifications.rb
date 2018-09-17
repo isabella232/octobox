826a14c033a20f2362a5eff02f024fd516af5ab5
jules2689 <julian@jnadeau.ca>
2018-09-17 13:36:53 -0400

    Fix migrations

--- db/migrate/20180913111742_add_muted_at_to_notifications.rb
+++ db/migrate/20180913111742_add_muted_at_to_notifications.rb
@@ -1,6 +1,11 @@
 class AddMutedAtToNotifications < ActiveRecord::Migration[5.2]
   def change
-    add_column :notifications, :muted_at, :datetime, null: true
-    add_index :notifications, :muted_at
+    unless column_exists?(:notifications, :muted_at)
+      add_column :notifications, :muted_at, :datetime, null: true
+    end
+
+    unless index_exists?(:notifications, :muted_at)
+      add_index :notifications, :muted_at
+    end
   end
 end
